87058a52482943af6dc384526e93628b2d4cb5ba
Samuel Echefu <mashce@uvic.ca>
2026-01-01 22:44:48 -0700

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
+        <ul>
+          {navLinks.map(({ label }) => {
+            return (
+              <li key={label}>
+                <a href={label}>{label}</a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="Search" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
