af1e2ff545c7f2768222141bcf53f7c0024ebcb1
Samuel Echefu <mashce@uvic.ca>
2026-01-01 23:05:26 -0700

    beginning to render threejs mac

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
 
 const App = () => {
   return <main>
     <Navbar />
     <Hero />
+    <ProductViewer />
   </main>;
 };
 
